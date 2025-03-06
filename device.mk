dafcbef9739e93606d899794ce615095a90e0855
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-03-07 04:36:01 +0530

    Add fingerprint and gatekeeper support to device.mk

--- device.mk
+++ device.mk
@@ -155,6 +155,12 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor:64
+
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service
 
 
 # AAPT
