e6ed19e6b724e074c39e25ca2dedd1fb6f7b457b
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-03-06 23:28:08 +0530

    Add AndroidProducts.mk for product configuration

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_LI9.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_LI9-userdebug \
+	lineage_LI9-eng \
+	lineage_LI9-user
