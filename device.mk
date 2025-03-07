54c5b6fa9b8b2684e61278ac62a8852c8a2e1dd7
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-03-07 06:16:08 +0530

    Add Mediatek thermal service and configuration to device.mk

--- device.mk
+++ device.mk
@@ -401,6 +401,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/transsion
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 
 
 
