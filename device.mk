bf15ce648218ffb6b0a7d2821cab09f292235238
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-03-07 05:52:47 +0530

    Add radio hardware packages to device.mk

--- device.mk
+++ device.mk
@@ -332,6 +332,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.6.vendor \
+    libprotobuf-cpp-full.vendor \
+    libprotobuf-cpp-lite.vendor \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 
 
 # AAPT
