6130fa4217601de03819964f83c830ee2294fa4e
CHIRO <90218247+shahqufi@users.noreply.github.com>
2025-03-08 16:16:46 +0530

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,9 +8,29 @@
 
 set -e
 
-# Define device-specific variables
-export DEVICE=LI9
-export VENDOR=tecno
+DEVICE=li9
+VENDOR=tecno
 
-# Run the setup-makefiles script from the device directory itself (since there's no common tree)
-"./../../${VENDOR}/${DEVICE}/setup-makefiles.sh" "$@"
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
