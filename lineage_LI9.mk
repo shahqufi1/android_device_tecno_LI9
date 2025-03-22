9ed85f6ab7f5f66ede6c61f8fd7bfae04eac23ba
shahqufi <shahqufi@gmail.com>
2025-03-23 01:04:49 +0530

    Add device-specific properties and configurations for TECNO LI9

--- lineage_LI9.mk
+++ lineage_LI9.mk
@@ -21,6 +21,37 @@ PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LI9
 
+# Lunch banner maintainer variable
+RISING_MAINTAINER="shahqufi"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# Set RISING_MAINTAINER for version control 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Dimensity 6080" \
+    RisingMaintainer="shahqufi"
+
+RISING_MAINTAINER := shahqufi
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := false
+
+# Whether to ship lawnchair launcher, false by default
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+WITH_GMS := true
+
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+TARGET_INCLUDE_GOOGLE_DIALER := true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2424
+TARGET_SCREEN_WIDTH := 1080
+
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
